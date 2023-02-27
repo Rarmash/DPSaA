31935ffdd7cd89f16ec17eb35a008c6b31bb6788
Andrew <62994051+Rarmash@users.noreply.github.com>
2023-02-27 16:41:27 +0300

    Update Практическое задание 1.cpp

--- Практическое задание 1/Практическое задание 1.cpp
+++ Практическое задание 1/Практическое задание 1.cpp
@@ -16,120 +16,242 @@ int digitroot(int n) {
     }
 }
 
-int* pasteelement(int n, int x[], int num, int pos)
+void pasteelement(int n, int x[], int num, int pos)
 {
     int i;
-    int* arr1 = new int[n];
-    for (int j = 0; j < n; j++) {
-        arr1[j] = x[j];
+    for (i = n; i > pos; i--) {
+        x[i] = x[i - 1];
     }
-    n++;
-    for (i = n; i >= pos; i--)
-        arr1[i] = arr1[i - 1];
-    arr1[pos - 1] = num;
-    return arr1;
+    x[pos] = num;
 }
 
-int* deleteelement(int* x, int n)
+void deleteelement(int x[], int n, int pos)
 {
-    int b[10], j = 0;
-    for (int i = 0; i < n; i++) {
-        if (digitroot(i) != 7) {
-            b[j] = x[i];
-            j++;
-        }
+    for (int i = pos; i < n; ++i)
+    {
+        x[i] = x[i + 1];
     }
-    for (int i = 0; i < j; i++) {
-        x[i] = b[i];
-    }
-    for (int i = j; i < 10; i++) {
-        x[i] = 0;
-    }
-    cout << endl;
-    for (int i = 0; i < j; i++) {
-        cout << x[i] << " ";
-    }
-    cout << endl;
-    return x;
 }
 
 bool is7(int k) {
     return digitroot(k) == 7;
 }
 
+int vibor0() {
+    cout << "Выберите тип массива:\n1. Статический\n2. Динамический\n3. С помощью <vector>" << endl;
+    int n;
+    cin >> n;
+    return n;
+}
+
+int vibor()
+{
+    int n;
+    cout << "Выберите номер задания" << endl;
+    cout << "1. Найти индекс элемента массива, цифровой корень которого равен 7." << endl;
+    cout << "2. Вставить новый элемент перед элементом, цифровой корень которого равен 7. Считать, что такое число одно." << endl;
+    cout << "3. Удалить элементы массива цифровой корень которых равен 7." << endl;
+    cin >> n;
+    return n;
+}
+
 int main()
 {
     setlocale(LC_ALL, "RU");
     cout << "Лабораторная работа №1 по теме Простые алгоритмы сортировки массивов. Гришин Андрей" << endl;
-    cout << "Выберите тип массива:\n1. Статический\n2. Динамический\n3. С помощью <vector>" << endl;
-    int n; cin >> n;
-    switch (n) {
+    switch (vibor0()) {
     case 1: {
-        const int ARRSIZE = 10;
-        int x[ARRSIZE];
-        for (int i = 0; i < ARRSIZE; i++) {
-            cin >> x[i];
-        }
-        int r = 1000000;
-        int k = 0;
-        for (int i = 0; i < ARRSIZE; i++) {
-            if (digitroot(i) == 7) {
-                if (i < r) {
-                    r = i;
+        switch (vibor()) {
+        case 1: {
+            const int ARRSIZE = 10;
+            int x[ARRSIZE];
+            cout << "Заполните 10 элементов массива: ";
+            for (int i = 0; i < ARRSIZE; i++) {
+                cin >> x[i];
+            }
+            bool k = false;
+            for (int i = 0; i < ARRSIZE; i++) {
+                if (digitroot(x[i]) == 7) {
+                    cout << "Цифровой корень равен 7 у элемента под индексом " << i;
+                    k = true;
                 }
-                k++;
             }
-        }
-        if (k == 0) {
-            r = 0;
-        }
-        if (r != 0) {
-            int* arr1 = pasteelement(ARRSIZE, x, 666, r);
-            int d = ARRSIZE + 1;
-            for (int i = 0; i < d; i++) {
-                cout << arr1[i] << " ";
+            if (k == false) {
+                cout << "Элементы с цифровым корнем 7 отсутствуют.";
             }
-            int* arr2 = deleteelement(arr1, d);
+            cout << endl;
+            break;
         }
-        else {
-            cout << "Ошибка. Введите другой массив.";
+        case 2: {
+            const int ARRSIZE = 10;
+            int x[ARRSIZE];
+            cout << "Заполните 10 элементов массива: ";
+            for (int i = 0; i < ARRSIZE; i++) {
+                cin >> x[i];
+            }
+            int y;
+            int r = 1000000;
+            int k = 0;
+            for (int i = 0; i < ARRSIZE; i++) {
+                if (digitroot(x[i]) == 7) {
+                    if (i < r) {
+                        r = i;
+                    }
+                    k++;
+                }
+            }
+            if (k == 0) {
+                r = 0;
+            }
+            cout << "Введите элемент для вставки: "; cin >> y;
+            if (r != 0) {
+                pasteelement(ARRSIZE, x, y, r);
+                int d = ARRSIZE + 1;
+                for (int i = 0; i < d; i++) {
+                    cout << x[i] << " ";
+                }
+            }
+            else {
+                cout << "Ошибка. Введите другой массив.";
+            }
+            break;
+        }
+        case 3: {
+            const int ARRSIZE = 10;
+            int x[ARRSIZE];
+            cout << "Заполните 10 элементов массива: ";
+            for (int i = 0; i < ARRSIZE; i++) {
+                cin >> x[i];
+            }
+            for (int i = 0; i < ARRSIZE; i++) {
+                if (digitroot(x[i]) == 7) {
+                    deleteelement(x, ARRSIZE, i);
+                }
+            }
+            for (int i = 0; i < ARRSIZE - 1; i++) {
+                cout << x[i] << " ";
+            }
+            break;
         }
         break;
+        }
     }
     case 2: {
-        int ARRSIZE;
-        cin >> ARRSIZE;
-        int* x = new int[ARRSIZE];
-        for (int i = 0; i < ARRSIZE; i++) {
-            cin >> x[i];
-        }
-        int r = 1000000;
-        int k = 0;
-        for (int i = 0; i < ARRSIZE; i++) {
-            if (digitroot(i) == 7) {
-                if (i < r) {
-                    r = i;
+        switch (vibor()) {
+        case 1: {
+            int ARRSIZE;
+            cout << "Введите размерность массива: ";
+            cin >> ARRSIZE;
+            int* x = new int[ARRSIZE];
+            cout << "Заполните " << ARRSIZE << " элементов массива: ";
+            for (int i = 0; i < ARRSIZE; i++) {
+                cin >> x[i];
+            }
+            bool k = false;
+            for (int i = 0; i < ARRSIZE; i++) {
+                if (digitroot(x[i]) == 7) {
+                    cout << "Цифровой корень равен 7 у элемента под индексом " << i << endl;
+                    k = true;
                 }
-                k++;
             }
-        }
-        if (k == 0) {
-            r = 0;
-        }
-        if (r != 0) {
-            int* arr1 = pasteelement(ARRSIZE, x, 666, r);
-            int d = ARRSIZE + 1;
-            for (int i = 0; i < d; i++) {
-                cout << arr1[i] << " ";
+            if (k == false) {
+                cout << "Элементы с цифровым корнем 7 отсутствуют.";
             }
-            int* arr2 = deleteelement(arr1, d);
+            cout << endl;
+            break;
         }
-        else {
-            cout << "Ошибка. Введите другой массив.";
+        case 2: {
+            int ARRSIZE;
+            cout << "Введите размерность массива: ";
+            cin >> ARRSIZE;
+            int* x = new int[ARRSIZE];
+            cout << "Заполните " << ARRSIZE << " элементов массива: ";
+            for (int i = 0; i < ARRSIZE; i++) {
+                cin >> x[i];
+            }
+            int y;
+            int r = 1000000;
+            int k = 0;
+            for (int i = 0; i < ARRSIZE; i++) {
+                if (digitroot(x[i]) == 7) {
+                    if (i < r) {
+                        r = i;
+                    }
+                    k++;
+                }
+            }
+            if (k == 0) {
+                r = 0;
+            }
+            cout << "Введите элемент для вставки: "; cin >> y;
+            if (r != 0) {
+                pasteelement(ARRSIZE, x, y, r);
+                int d = ARRSIZE + 1;
+                for (int i = 0; i < d; i++) {
+                    cout << x[i] << " ";
+                }
+            }
+            else {
+                cout << "Ошибка. Введите другой массив.";
+            }
+            break;
+        }
+        case 3: {
+            int ARRSIZE;
+            cout << "Введите размерность массива: ";
+            cin >> ARRSIZE;
+            int* x = new int[ARRSIZE];
+            cout << "Заполните " << ARRSIZE << " элементов массива: ";
+            for (int i = 0; i < ARRSIZE; i++) {
+                cin >> x[i];
+            }
+            for (int i = 0; i < ARRSIZE; i++) {
+                if (digitroot(x[i]) == 7) {
+                    deleteelement(x, ARRSIZE, i);
+                }
+            }
+            for (int i = 0; i < ARRSIZE - 1; i++) {
+                cout << x[i] << " ";
+            }
+            break;
         }
         break;
+        }
     }
     case 3: {
+        switch (vibor()) {
+        case 1: {
+            int ARRSIZE;
+            cout << "Введите размерность массива: ";
+            cin >> ARRSIZE;
+            vector<int> x(ARRSIZE);
+            cout << "Заполните " << ARRSIZE << " элементов массива: ";
+            for (int i = 0; i < ARRSIZE; i++) {
+                cin >> x[i];
+            }
+            bool k = false;
+            for (int i = 0; i < ARRSIZE; i++) {
+                if (digitroot(x[i]) == 7) {
+                    cout << "Цифровой корень равен 7 у элемента под индексом " << i;
+                    k = true;
+                }
+            }
+            if (k == false) {
+                cout << "Элементы с цифровым корнем 7 отсутствуют.";
+            }
+            cout << endl;
+            break;
+        }
+        case 2: {
+
+        }
+        case 3: {
+
+        }
+        break;
+        }
+
+        /*
         int ARRSIZE;
         cin >> ARRSIZE;
         vector<int> x(ARRSIZE);
@@ -165,6 +287,7 @@ int main()
             cout << "Ошибка. Введите другой массив.";
         }
         break;
+        */
     }
     default: {
         cout << "Вы ввели неверный номер.";
